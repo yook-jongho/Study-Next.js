0b2ce00370675f0a1b08588db5aa3ea2edba59ef
육종호 <dbrwhdgh1203@naver.com>
2023-08-16 15:42:42 +0900

    use static asset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ export default function Home() {
         <>
             <h2>Welcome</h2>
             Hello, WEB
+            <p>
+                <img src="/next.svg" width="80" alt="" />
+                <br></br>
+                <img src="/vercel.svg" width="80" alt="" />
+            </p>
         </>
     );
 }
