b8c2805f9545b470b79466c9e6a4cf7ae83d7068
육종호 <dbrwhdgh1203@naver.com>
2023-08-26 16:09:04 +0900

    code refactoring

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,46 +1,55 @@
 "use client"; //위의 코드를 사용하면 client component로 전환
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Create() {
     const router = useRouter(); //useRouter 객체 생성
-    return (
-        <form
-            onSubmit={async (evt: React.FormEvent<HTMLFormElement>) => {
-                evt.preventDefault();
-                const form = evt.target as HTMLFormElement;
-                const title = form.elements.namedItem(
-                    "title"
-                ) as HTMLInputElement;
-                const body = form.elements.namedItem(
-                    "body"
-                ) as HTMLTextAreaElement;
-                const resp = await fetch("http://localhost:9999/topics/", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                        title: title.value,
-                        body: body.value,
-                    }),
-                });
-                const topic = await resp.json();
-                console.log("file: page.js:19 ~ Create ~ topic:", topic);
+    const [title, setTitle] = useState("");
+    const [body, setBody] = useState("");
 
-                // router.push를 사용하면 페이지 리로드 없이 사용자의 화면을 해당 페이지로 이동합니다.
-                //router.refresh를 사용하면 서버 컴포넌트를 서버 쪽에서 다시 랜더링해서 새로 고침할 수 있습니다.
-                //여기서는 app/layout.js을 새로고침하기 위해서 사용된 코드입니다.
-                router.push(`/read/${topic.id}`);
-                router.refresh();
-            }}
-        >
+    const handleSubmit = async (evt: React.FormEvent) => {
+        evt.preventDefault();
+        const resp = await fetch(`http://localhost:9999/topics/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title, body }),
+        });
+        const topic = await resp.json();
+        console.log("file: page.js:19 ~ Create ~ topic:", topic);
+
+        // router.push를 사용하면 페이지 리로드 없이 사용자의 화면을 해당 페이지로 이동합니다.
+        // router.refresh를 사용하면 서버 컴포넌트를 서버 쪽에서 다시 랜더링해서 새로 고침할 수 있습니다.
+        // 여기서는 app/layout.js을 새로고침하기 위해서 사용된 코드입니다.
+        router.push(`/read/${topic.id}`);
+        router.refresh();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
             <h2>Create</h2>
             <p>
-                <input type="text" name="title" placeholder="title" />
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="title"
+                    value={title}
+                    onChange={(e) => {
+                        setTitle(e.target.value);
+                    }}
+                />
             </p>
             <p>
-                <textarea name="body" placeholder="body"></textarea>
+                <textarea
+                    name="body"
+                    placeholder="body"
+                    value={body}
+                    onChange={(e) => {
+                        setBody(e.target.value);
+                    }}
+                ></textarea>
             </p>
             <p>
                 <input type="submit" value="create" />
